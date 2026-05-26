422cb2c99212f9b99a7eba5ef3b655956531c70b
motafreitas29-cmyk <motafreitas29@gmail.com>
2026-05-25 23:01:11 -0300

    feat: declare lite-vsl custom element

---
+++ src/custom-elements.d.ts
@@ -0,0 +1,5 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'lite-vsl': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { id?: string; base?: string; aspect?: string }, HTMLElement>;
+  }
+}
