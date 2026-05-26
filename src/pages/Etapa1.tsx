10c79843c182553c5d525eb827408e99ca11a854
motafreitas29-cmyk <motafreitas29@gmail.com>
2026-05-25 22:35:30 -0300

    feat: add product image to Etapa1

--- src/pages/Etapa1.tsx
+++ src/pages/Etapa1.tsx
@@ -21,7 +21,7 @@ export default function Etapa1({ onNext }: Props) {
 
         <div className="mb-8 rounded-2xl overflow-hidden border border-[#C4424A]/30 shadow-[0_0_30px_rgba(196,66,74,0.15)]">
           <img
-            src="TROCAR_PELA_URL_DA_IMAGEM"
+            src="https://i.postimg.cc/PJPGL7rk/a9dd8ffc-acd5-48bf-bed0-484914e1fe7d-(1).png"
             alt="Guia Definitivo da Colorimetria Labial"
             className="w-full h-auto object-cover"
           />
